5828a83557d323fbf6074d7a18429eb8b3503f24
David Friedl <david.friedl@whu.edu>
2019-05-16 16:57:32 +0200

    Backend functionality complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'open-uri'
+drinks_hash = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+
+
+drinks_hash["drinks"].each do |drink|
+  Ingredient.create(name: drink["strIngredient1"])
+end
